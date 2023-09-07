e50d00c92bbb205f3ebf3354e98101f36825254b
suzuki_masato <k020g1194@g.neec.ac.jp>
2023-09-07 23:16:57 +0900

    曲がる時滑らかにした　十字やダブルカーブにも対応できるようにした。

--- slide puzzle/Player.cpp
+++ slide puzzle/Player.cpp
@@ -2,6 +2,7 @@
 #include<Shape.h>
 #include<Collision.h>
 #include"plate.h"
+#include"Easing.h"
 Player::Player()
 {
 }
@@ -12,39 +13,56 @@ Player::~Player()
 
 void Player::Init()
 {
+	pObject = Shape::CreateOBJ("car");
 
-	pObject = Shape::CreateOBJ("sphere");
+	const Vec2 basePos = { -9.0f,6.0f };
+	const Vec2 varPos = { 6.1f,-6.1f };
 
+	position = Vec3(basePos.x + 0 * varPos.x, 1.5f, basePos.y + 1 * varPos.y + varPos.y / 2);
+	positionMemory = position;
 }
 
 void Player::Update(Plate* plate)
 {
-
 	DirectChange(plate);
-
 	Move();
-
 }
 
 void Player::Draw()
 {
 
-	Object::Draw(pObject, position, Vec3(1.0f, 1.0f, 1.0f), Vec3());
+	Object::Draw(pObject, position, Vec3(1.0f, 1.0f, 1.0f), rotation);
 }
 
 void Player::DirectChange(Plate* plate)
 {
-	//プレート基礎位置
-	const Vec2 basePos = { -9.0f,6.0f };
-	const Vec2 varPos = { 6.1f,-6.1f };
-	Vec2 pSize = { 0.01f,0.01f }, sSize = { 6.1f,6.1f };
+	bool hit = false;
+	if (isPosFlag == false && isPosSecondFlag == false)
+	{
+		if (direction == UP)
+		{
+			position.z += 0.2f;
+		}
+		if (direction == DOWN)
+		{
+			position.z -= 0.2f;
+		}
+		if (direction == RIGHT)
+		{
+			position.x += 0.2f;
+		}
+		if (direction == LEFT)
+		{
+			position.x -= 0.2f;
+		}
+	}
 	for (int j = 0; j < 3; j++)
 	{
 		for (int i = 0; i < 4; i++)
 		{
 			//当たり判定
 			if (Collision::BoxCollision(
-				Vec2(position.x, position.z), Vec2(basePos.x + i * varPos.x, basePos.y + j * varPos.y),
+				Vec2(position.x, position.z), Vec2(basePos.x + i * varPos.x, basePos.y + j * -varPos.y),
 				pSize / 2, sSize / 2))
 			{
 				switch (plate->GetStage(i, j))
@@ -53,106 +71,297 @@ void Player::DirectChange(Plate* plate)
 					isDead = true;
 					break;
 				case WIDTHSTRAIGHTLINE://横直線
-					if (direction == UP || DOWN) { isDead = true; }
-					if (direction == LEFTDOWN || direction == LEFTUP)
-					{
-						direction = LEFT;
-						position.z = basePos.y + j * varPos.y;
-					}
-					if (direction == RIGHTDOWN || direction == RIGHTUP)
-					{
-						direction = RIGHT;
-						position.z = basePos.y + j * varPos.y;
-					}
+					LineInit();
+					TurnLeft();
+					TurnRight();
 					break;
 				case HEIGHTSTRAIGHTLINE://縦直線
-					if (direction == LEFT || RIGHT) { isDead = true; }
-					if (direction == LEFTDOWN || direction == RIGHTDOWN)
-					{
-						direction = DOWN;
-						position.x = basePos.x + i * varPos.x;
-					}
-					if (direction == LEFTUP || direction == RIGHTUP)
-					{
-						direction = UP;
-						position.x = basePos.x + i * varPos.x;
-					}
+					LineInit();
+					TurnUp();
+					TurnDown();
 					break;
 				case CROSS:            //十字
+					LineInit();
+					TurnLeft();
+					TurnRight();
+					TurnUp();
+					TurnDown();
 					break;
-				case CUR_LEFTUP:       //カーブ左と上
-					if (direction == LEFT || direction == UP) { isDead = true; }
-					if (direction == DOWN)
-					{
-						direction = LEFTDOWN;
-					}
-					if (direction == RIGHT)
-					{
-						direction = RIGHTUP;
-					}
+				case CUR_LEFTUP:       //カーブ左と上	
+					TurnInit();
+					TurnDownLeft();
+					TurnRightUp();
 					break;
-				case CUR_LEFTDOWN:	       //カーブ左と下
-					if (direction == LEFT || direction == DOWN) { isDead = true; }
-					if (direction == UP)
-					{
-						direction = LEFTUP;
-					}
-					if (direction == RIGHT)
-					{
-						direction = RIGHTDOWN;
-					}
+				case CUR_LEFTDOWN:	   //カーブ左と下
+					TurnInit();
+					TurnRightDown();
+					TurnUpLeft();
 					break;
-				case CUR_RIGHTUP:          //カーブ右と上
-					if (direction == RIGHT || direction == UP) { isDead = true; }
-					if (direction == DOWN)
-					{
-						direction = RIGHTDOWN;
-					}
-					if (direction == LEFT)
-					{
-						direction = LEFTUP;
-					}
+				case CUR_RIGHTUP:      //カーブ右と上
+					TurnInit();
+					TurnLeftUp();
+					TurnDownRight();
 					break;
-				case CUR_RIGHTDOWN:        //カーブ右と下
-					if (direction == RIGHT || direction == DOWN) { isDead = true; }
-					if (direction == UP)
-					{
-						direction = RIGHTUP;
-					}
-					if (direction == LEFT)
-					{
-						direction = LEFTDOWN;
-					}
+				case CUR_RIGHTDOWN:    //カーブ右と下
+					TurnInit();
+					TurnLeftDown();
+					TurnUpRight();
 					break;
 				case DCUR_LEFTUP_RIGHTDOWN://ダブルカーブ左と上＆右と下
+					TurnInit();
+					TurnDownLeft();
+					TurnRightUp();
+					TurnLeftDown();
+					TurnUpRight();
 					break;
 				case DCUR_LEFTDOWN_RIGHTUP://ダブルカーブ左と下＆右と上
+					TurnInit();
+					TurnLeftUp();
+					TurnDownRight();
+					TurnRightDown();
+					TurnUpLeft();
 					break;
 				default:
 					break;
 				}
+				hit = true;
 			}
 		}
+	}
+	//タイル外にいったら死亡
+	if (hit == false)
+	{
+		isDead = true;
 	}
 }
 
 void Player::Move()
 {
+	if (isDead == true) { return; }
 
-	if (direction == UP || direction == LEFTUP || direction == RIGHTUP)
+	if (isPosFlag == true)
 	{
-		position.z += speed;
+		//移動の処理
+		position = Easing::lerp(posStart, posEnd, plateTime);
+		//プレイヤーが回転する処理
+		if (isCurFlag == true)
+		{
+			rotation = Easing::lerp(turnStart, turnEnd, plateTime);
+		}
+		plateTime += turnSpeed;
+		//移動終わったら
+		if (plateTime >= 1.0f)
+		{
+			posStartSecond = position;
+			turnStartSecond = rotation;
+			isPosFlag = false;
+			plateTime = 0.0f;
+		}
 	}
-	if (direction == DOWN || direction == LEFTDOWN || direction == RIGHTDOWN)
+	else if (isPosSecondFlag == true)
 	{
-		position.z -= speed;
-	}
-	if (direction == LEFT || direction == LEFTDOWN || direction == LEFTUP)
-	{
-		position.x -= speed;
-	}
-	if (direction == RIGHT || direction == RIGHTDOWN || direction == RIGHTUP)
-	{
-		position.x += speed;
+		//移動の処理
+		position = Easing::lerp(posStartSecond, posEndSecond, plateTime);
+		//プレイヤーが回転する処理
+		if (isCurFlag == true)
+		{
+			rotation = Easing::lerp(turnStartSecond, turnEndSecond, plateTime);
+		}
+		plateTime += turnSpeed;
+		//移動終わったら
+		if (plateTime >= 1.0f)
+		{
+			isPosSecondFlag = false;
+			isCurFlag = false;
+			plateTime = 0.0f;
+		}
 	}
 }
+
+void Player::TurnInit()
+{
+	if (isPosFlag == true || isPosSecondFlag == true) { return; }
+	posStart = position;
+	turnStart = rotation;
+	isCurFlag = true;
+	isPosFlag = true, isPosSecondFlag = true;
+	posEndSecond = {};
+}
+
+void Player::LineInit()
+{
+	if (isPosFlag == true || isPosSecondFlag == true) { return; }
+	posStart = position;
+	isPosFlag = true, isPosSecondFlag = true;
+	posEndSecond = {};
+}
+
+void Player::TurnLeft()
+{
+	if (direction != LEFT) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		position = positionMemory;
+		posEnd = Vec3(position.x - varPos.x / 2, position.y, position.z);
+		posEndSecond = Vec3(position.x - varPos.x, position.y, position.z);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnRight()
+{
+	if (direction != RIGHT) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		position = positionMemory;
+		posEnd = Vec3(position.x + varPos.x / 2, position.y, position.z);
+		posEndSecond = Vec3(position.x + varPos.x, position.y, position.z);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnUp()
+{
+	if (direction != UP) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		position = positionMemory;
+		posEnd = Vec3(position.x, position.y, position.z + varPos.y / 2);
+		posEndSecond = Vec3(position.x, position.y, position.z + varPos.y);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnDown()
+{
+	if (direction != DOWN) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		position = positionMemory;
+		posEnd = Vec3(position.x, position.y, position.z - varPos.y / 2);
+		posEndSecond = Vec3(position.x, position.y, position.z - varPos.y);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnLeftDown()
+{
+	if (direction != LEFT) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		position = positionMemory;
+		direction = DOWN;
+		turnEnd = Vec3(rotation.x, rotation.y - 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y - 90.0f, rotation.z);
+		posEnd = Vec3(position.x - varPos.x / 4, position.y, position.z - varPos.y / 4);
+		posEndSecond = Vec3(position.x - varPos.x / 2, position.y, position.z - varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnUpRight()
+{
+	if (direction != UP) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		direction = RIGHT;
+		position = positionMemory;
+		turnEnd = Vec3(rotation.x, rotation.y + 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y + 90.0f, rotation.z);
+		posEnd = Vec3(position.x + varPos.x / 4, position.y, position.z + varPos.y / 4);
+		posEndSecond = Vec3(position.x + varPos.x / 2, position.y, position.z + varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnRightDown()
+{
+	if (direction != RIGHT) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		direction = DOWN;
+		position = positionMemory;
+		turnEnd = Vec3(rotation.x, rotation.y + 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y + 90.0f, rotation.z);
+
+		posEnd = Vec3(position.x + varPos.x / 4, position.y, position.z - varPos.y / 4);
+		posEndSecond = Vec3(position.x + varPos.x / 2, position.y, position.z - varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnUpLeft()
+{
+	if (direction != UP) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		direction = LEFT;
+		position = positionMemory;
+		turnEnd = Vec3(rotation.x, rotation.y - 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y - 90.0f, rotation.z);
+		posEnd = Vec3(position.x - varPos.x / 4, position.y, position.z + varPos.y / 4);
+		posEndSecond = Vec3(position.x - varPos.x / 2, position.y, position.z + varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnDownLeft()
+{
+	if (direction != DOWN) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		direction = LEFT;
+		position = positionMemory;
+		turnEnd = Vec3(rotation.x, rotation.y + 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y + 90.0f, rotation.z);
+		posEnd = Vec3(position.x - varPos.x / 4, position.y, position.z - varPos.y / 4);
+		posEndSecond = Vec3(position.x - varPos.x / 2, position.y, position.z - varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnRightUp()
+{
+	if (direction != RIGHT) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		direction = UP;
+		position = positionMemory;
+		turnEnd = Vec3(rotation.x, rotation.y - 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y - 90.0f, rotation.z);
+		posEnd = Vec3(position.x + varPos.x / 4, position.y, position.z + varPos.y / 4);
+		posEndSecond = Vec3(position.x + varPos.x / 2, position.y, position.z + varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnLeftUp()
+{
+	if (direction != LEFT) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		direction = UP;
+		position = positionMemory;
+		turnEnd = Vec3(rotation.x, rotation.y + 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y + 90.0f, rotation.z);
+		posEnd = Vec3(position.x - varPos.x / 4, position.y, position.z + varPos.y / 4);
+		posEndSecond = Vec3(position.x - varPos.x / 2, position.y, position.z + varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
+void Player::TurnDownRight()
+{
+	if (direction != DOWN) { return; }
+	if (posEndSecond.x == 0.0f && posEndSecond.y == 0.0f && posEndSecond.z == 0.0f)
+	{
+		direction = RIGHT;
+		position = positionMemory;
+		turnEnd = Vec3(rotation.x, rotation.y - 45.0f, rotation.z);
+		turnEndSecond = Vec3(rotation.x, rotation.y - 90.0f, rotation.z);
+		posEnd = Vec3(position.x + varPos.x / 4, position.y, position.z - varPos.y / 4);
+		posEndSecond = Vec3(position.x + varPos.x / 2, position.y, position.z - varPos.y / 2);
+		positionMemory = posEndSecond;
+	}
+}
+
