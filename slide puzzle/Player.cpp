318bb28489767c4cc66c4d4c8aab830898987b72
suzuki_masato <k020g1194@g.neec.ac.jp>
2023-09-11 13:32:42 +0900

    スコア描画、スコア計算

--- slide puzzle/Player.cpp
+++ slide puzzle/Player.cpp
@@ -15,7 +15,9 @@ void Player::Init()
 {
 	pObject = Shape::CreateOBJ("car");
 
-
+	scoreDraw = Shape::CreateRect(1.0f, 1.0f);
+	backSprite = Sprite::Get()->SpriteCreate(L"Resources/scoreBack.png");
+	comboSprite = Sprite::Get()->SpriteCreate(L"Resources/combo.png");
 	numberGraph[0] = Sprite::Get()->SpriteCreate(L"Resources/number/0.png");
 	numberGraph[1] = Sprite::Get()->SpriteCreate(L"Resources/number/1.png");
 	numberGraph[2] = Sprite::Get()->SpriteCreate(L"Resources/number/2.png");
@@ -26,6 +28,18 @@ void Player::Init()
 	numberGraph[7] = Sprite::Get()->SpriteCreate(L"Resources/number/7.png");
 	numberGraph[8] = Sprite::Get()->SpriteCreate(L"Resources/number/8.png");
 	numberGraph[9] = Sprite::Get()->SpriteCreate(L"Resources/number/9.png");
+
+	comboGraph[0] = Sprite::Get()->SpriteCreate(L"Resources/number/combo0.png");
+	comboGraph[1] = Sprite::Get()->SpriteCreate(L"Resources/number/combo1.png");
+	comboGraph[2] = Sprite::Get()->SpriteCreate(L"Resources/number/combo2.png");
+	comboGraph[3] = Sprite::Get()->SpriteCreate(L"Resources/number/combo3.png");
+	comboGraph[4] = Sprite::Get()->SpriteCreate(L"Resources/number/combo4.png");
+	comboGraph[5] = Sprite::Get()->SpriteCreate(L"Resources/number/combo5.png");
+	comboGraph[6] = Sprite::Get()->SpriteCreate(L"Resources/number/combo6.png");
+	comboGraph[7] = Sprite::Get()->SpriteCreate(L"Resources/number/combo7.png");
+	comboGraph[8] = Sprite::Get()->SpriteCreate(L"Resources/number/combo8.png");
+	comboGraph[9] = Sprite::Get()->SpriteCreate(L"Resources/number/combo9.png");
+	comboGraph[10] = Sprite::Get()->SpriteCreate(L"Resources/number/plus.png");
 
 	const Vec2 basePos = { -9.0f,6.0f };
 	const Vec2 varPos = { 6.1f,-6.1f };
@@ -39,6 +53,7 @@ void Player::Update(Plate* plate)
 	DirectChange(plate);
 	Move();
 	CrstalGet(plate);
+	PointUpdate();
 }
 
 void Player::Draw()
@@ -49,15 +64,44 @@ void Player::Draw()
 
 void Player::ScoreDraw()
 {
-
-	int pointOne, pointTwo, pointThree;
-	pointOne = pointNum % 10;
-	pointTwo = pointNum % 100 / 10;
-	pointThree = pointNum / 100;
-
-	Sprite::Get()->Draw(numberGraph[pointOne], Vec2(80.0f, 0.0f), 60, 60);
-	Sprite::Get()->Draw(numberGraph[pointTwo], Vec2(40.0f,0.0f), 60, 60);
-	Sprite::Get()->Draw(numberGraph[pointThree], Vec2(), 60, 60);
+	//3D
+	if (isScoreDraw == true)
+	{
+		int pointPlasNum[3] = {};
+		Object::Draw(scoreDraw, scorePos + Vec3(0.0f, 0.0f, 3.0f),
+			Vec3(5.5f, 1.1f, 1.1f), Vec3(90.0f, 0.0f, 0.0f), comboSprite.texNumber);
+		Object::Draw(scoreDraw, scorePos + Vec3(-2.0f, 0.0f, 1.5f),
+			Vec3(1.0f, 1.0f, 1.0f), Vec3(90.0f, 0.0f, 0.0f), comboGraph[10].texNumber);
+		int point = 0;
+		if (pointPlas != 100) { point = pointPlas - 10; }
+		else { point = pointPlas; }
+		pointPlasNum[0] = point % 10;
+		pointPlasNum[1] = point % 100 / 10;
+		pointPlasNum[2] = point % 1000 / 100;
+		Vec3 plasPos = { 1.0f,0.0f,1.5f };
+		for (int i = 0; i < 3; i++)
+		{
+			if (i == 2 && point != 100) { break; }
+			Object::Draw(scoreDraw, scorePos + plasPos,
+				Vec3(1.0f, 1.0f, 1.0f), Vec3(90.0f, 0.0f, 0.0f), comboGraph[pointPlasNum[i]].texNumber);
+			plasPos += Vec3(-1.0f, 0.0f, 0.0f);
+		}
+	}
+	int pointNumber[5] = {};
+	pointNumber[0] = pointNum % 10;
+	pointNumber[1] = pointNum % 100 / 10;
+	pointNumber[2] = pointNum % 1000 / 100;
+	pointNumber[3] = pointNum % 10000 / 1000;
+	pointNumber[4] = pointNum % 100000 / 10000;
+	//2D描画
+	Sprite::Get()->Draw(backSprite, Vec2(), 250, 80);
+	Vec2 posPlas = { 40.0f,0.0f };
+	int j = 0;
+	for (int i = 4; i > -1; i--)
+	{
+		Sprite::Get()->Draw(numberGraph[pointNumber[i]], Vec2() + posPlas * static_cast<float>(j), 60, 60);
+		j++;
+	}
 }
 
 void Player::Reset()
@@ -80,10 +124,15 @@ void Player::Reset()
 
 void Player::PointUpdate()
 {
-
-
-	pointNum += pointPlas;
-
+	//コンボ
+	if (comboTime > 0) { comboTime--; }
+	if (comboTime == 0) { pointPlas = 10; }
+	//ポイント
+	if (pointPlas > 100) { pointPlas = 100; }
+	if (pointNum > 99999) { pointNum = 99999; }
+	//スコア
+	if (scoreDrawTime > 0) { scoreDrawTime--; }
+	else { isScoreDraw = false; }
 }
 
 void Player::DirectChange(Plate* plate)
@@ -265,7 +314,12 @@ void Player::CrstalGet(Plate* plate)
 			pSize / 2, crstalSize / 2))
 		{
 			pointNum += pointPlas;
+			pointPlas += 10;
+			comboTime = comboTimeMax;
 			turnSpeed += speedPlas;
+			isScoreDraw = true;
+			scorePos = position;
+			scoreDrawTime = scoreDrawTimeMax;
 			//クリスタルが消える処理
 			plate->DeleteCrstal(i);
 		}
@@ -275,7 +329,6 @@ void Player::CrstalGet(Plate* plate)
 void Player::TurnInit()
 {
 	if (isPosFlag == true || isPosSecondFlag == true) { return; }
-	//posStart = position;
 	turnStart = rotation;
 	isCurFlag = true;
 	isPosFlag = true, isPosSecondFlag = true;
@@ -285,7 +338,6 @@ void Player::TurnInit()
 void Player::LineInit()
 {
 	if (isPosFlag == true || isPosSecondFlag == true) { return; }
-	//posStart = position;
 	isPosFlag = true, isPosSecondFlag = true;
 	posEndSecond = {};
 }
--- slide puzzle/Player.h
+++ slide puzzle/Player.h
@@ -50,7 +50,7 @@ public:
 	bool GetDeadFlag() { return isDead; }
 private:
 	/// <summary>
-	/// ポイント加算
+	/// コンボ
 	/// </summary>
 	void PointUpdate();
 	/// <summary>
@@ -138,9 +138,21 @@ private:
 
 
 	//コンボ関連
+	SpriteData backSprite;
+	SpriteData comboSprite;
 	SpriteData numberGraph[10] = {};
-	int pointNum = 0;  //コンボ
-	int pointPlas = 10;//コンボ加算
+	const int comboTimeMax = 400;//コンボ最大
+	int comboTime = 0;           //コンボ時間
+	int pointNum = 0;            //コンボ
+	int pointPlas = 10;          //コンボ加算
+
+	//スコア描画
+	ObjectData scoreDraw = {};
+	SpriteData comboGraph[11] = {};
+	Vec3 scorePos = {};
+	bool isScoreDraw = false;
+	const int scoreDrawTimeMax = 50;
+	int scoreDrawTime = 0;
 
 
 	//クリスタル関連
